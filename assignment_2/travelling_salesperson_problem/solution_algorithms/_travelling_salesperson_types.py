4177ddf60351255d78371d42b3f094bcfd30075f
ffekirnew <ffekirnew0808@gmail.com>
2023-05-16 08:28:15 +0300

    tried to work on the tsp problem

---
+++ assignment_2/travelling_salesperson_problem/solution_algorithms/_travelling_salesperson_types.py
@@ -0,0 +1,26 @@
+from typing import List
+
+# General Types
+City = str
+Weight = int
+Graph = dict[dict[Weight]]
+romanian_map = {
+    'Arad': {'Zerind': 75, 'Sibiu': 140, 'Timisoara': 118},
+    'Zerind': {'Arad': 75, 'Oradea': 71},
+    'Oradea': {'Zerind': 71, 'Sibiu': 151},
+    'Sibiu': {'Arad': 140, 'Oradea': 151, 'Fagaras': 99, 'Rimnicu Vilcea': 80},
+    'Timisoara': {'Arad': 118, 'Lugoj': 111},
+    'Lugoj': {'Timisoara': 111, 'Mehadia': 70},
+    'Mehadia': {'Lugoj': 70, 'Drobeta': 75},
+    'Drobeta': {'Mehadia': 75, 'Craiova': 120},
+    'Craiova': {'Drobeta': 120, 'Rimnicu Vilcea': 146, 'Pitesti': 138},
+    'Rimnicu Vilcea': {'Sibiu': 80, 'Craiova': 146, 'Pitesti': 97},
+    'Fagaras': {'Sibiu': 99, 'Bucharest': 211},
+    'Pitesti': {'Rimnicu Vilcea': 97, 'Craiova': 138, 'Bucharest': 101},
+    'Bucharest': {'Fagaras': 211, 'Pitesti': 101}
+}
+
+# Simulated Annealing Types
+Energy = int
+Tour = List[City]
+Temperature = int
