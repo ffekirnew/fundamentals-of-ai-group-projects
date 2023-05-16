b02de9b7dd3e27b50f56bc55dcb2f0ec37299cfd
ffekirnew <ffekirnew0808@gmail.com>
2023-05-16 22:43:52 +0300

    Tried to work on the simulated annealing algorithm solution for the tsp

--- local-search-algorithms/travelling_salesperson_problem/solution_algorithms/_travelling_salesperson_types.py
+++ local-search-algorithms/travelling_salesperson_problem/solution_algorithms/_travelling_salesperson_types.py
@@ -1,26 +1,61 @@
-from typing import List
+import heapq
 
-# General Types
-City = str
-Weight = int
-Graph = dict[dict[Weight]]
-romanian_map = {
-    'Arad': {'Zerind': 75, 'Sibiu': 140, 'Timisoara': 118},
-    'Zerind': {'Arad': 75, 'Oradea': 71},
-    'Oradea': {'Zerind': 71, 'Sibiu': 151},
-    'Sibiu': {'Arad': 140, 'Oradea': 151, 'Fagaras': 99, 'Rimnicu Vilcea': 80},
-    'Timisoara': {'Arad': 118, 'Lugoj': 111},
-    'Lugoj': {'Timisoara': 111, 'Mehadia': 70},
-    'Mehadia': {'Lugoj': 70, 'Drobeta': 75},
-    'Drobeta': {'Mehadia': 75, 'Craiova': 120},
-    'Craiova': {'Drobeta': 120, 'Rimnicu Vilcea': 146, 'Pitesti': 138},
-    'Rimnicu Vilcea': {'Sibiu': 80, 'Craiova': 146, 'Pitesti': 97},
-    'Fagaras': {'Sibiu': 99, 'Bucharest': 211},
-    'Pitesti': {'Rimnicu Vilcea': 97, 'Craiova': 138, 'Bucharest': 101},
-    'Bucharest': {'Fagaras': 211, 'Pitesti': 101}
-}
 
-# Simulated Annealing Types
-Energy = int
-Tour = List[City]
-Temperature = int
+def ucs(graph, start, end):
+    queue = [(0, start, [])]
+    visited = set()
+
+    while queue:
+        cost, city, path = heapq.heappop(queue)
+
+        if city == end:
+            return path + [city]
+
+        if city in visited:
+            continue
+
+        visited.add(city)
+
+        for neighbor, edge_cost in graph[city]:
+            if neighbor not in visited:
+                heapq.heappush(queue, (cost + edge_cost, neighbor, path + [city]))
+
+    return None
+
+
+if __name__ == "__main__":
+    # Romanian map represented as an adjacency list
+    graph = {
+        'Arad': [('Zerind', 75), ('Timisoara', 118), ('Sibiu', 140)],
+        'Zerind': [('Arad', 75), ('Oradea', 71)],
+        'Timisoara': [('Arad', 118), ('Lugoj', 111)],
+        'Sibiu': [('Arad', 140), ('Fagaras', 99), ('Rimnicu Vilcea', 80), ('Oradea', 151)],
+        'Oradea': [('Zerind', 71), ('Sibiu', 151)],
+        'Lugoj': [('Timisoara', 111), ('Mehadia', 70)],
+        'Fagaras': [('Sibiu', 99), ('Bucharest', 211)],
+        'Rimnicu Vilcea': [('Sibiu', 80), ('Pitesti', 97), ('Craiova', 146)],
+        'Mehadia': [('Lugoj', 70), ('Dobreta', 75)],
+        'Bucharest': [('Fagaras', 211), ('Pitesti', 101), ('Giurgiu', 90), ('Urziceni', 85)],
+        'Pitesti': [('Rimnicu Vilcea', 97), ('Bucharest', 101), ('Craiova', 138)],
+        'Craiova': [('Rimnicu Vilcea', 146), ('Pitesti', 138), ('Dobreta', 120)],
+        'Dobreta': [('Mehadia', 75), ('Craiova', 120)],
+        'Giurgiu': [('Bucharest', 90)],
+        'Urziceni': [('Bucharest', 85), ('Hirsova', 98), ('Vaslui', 142)],
+        'Hirsova': [('Urziceni', 98), ('Eforie', 86)],
+        'Vaslui': [('Urziceni', 142), ('Iasi', 92)],
+        'Iasi': [('Vaslui', 92), ('Neamt', 87)],
+        'Neamt': [('Iasi', 87)],
+        'Eforie': [('Hirsova', 86)]
+    }
+
+    start_city = 'Arad'
+    end_city = 'Bucharest'
+
+    # Find the shortest path between two cities using UCS
+    path = ucs(graph, start_city, end_city)
+
+    if path:
+        print(f"Shortest path from {start_city} to {end_city}:")
+        print(" -> ".join(path))
+    else:
+        print(f"No path found from {start_city} to {end_city}.")
---
+++ local-search-algorithms/travelling_salesperson_problem/solution_algorithms/hill_climbing_algorithm.py
@@ -0,0 +1,132 @@
+import heapq
+import random
+from math import inf
+
+
+class HillClimbingAlgorithm:
+    def __init__(self, map, cities):
+        self.map = map
+        self.cities = cities
+        self.distances = self.all_distances()
+
+    def dijkstra(self, start):
+        graph = self.map
+
+        distances = {city: float('inf') for city in graph}
+        distances[start] = 0
+        queue = [(0, start)]
+
+        while queue:
+            current_distance, current_city = heapq.heappop(queue)
+
+            if current_distance > distances[current_city]:
+                continue
+
+            for neighbor, weight in graph[current_city]:
+                distance = current_distance + weight
+
+                if distance < distances[neighbor]:
+                    distances[neighbor] = distance
+                    heapq.heappush(queue, (distance, neighbor))
+
+        return distances
+
+    def all_distances(self):
+        all_distances = {}
+        for city in self.cities:
+            distances = self.dijkstra(city)
+            all_distances[city] = distances
+
+        return all_distances
+
+    def generate_initial_solution(self):
+        return random.sample(self.cities, len(self.cities))
+
+    def distance(self, city1, city2):
+        return self.distances[city1][city2]
+
+    def calculate_total_distance(self, route):
+        total = 0
+        for i in range(len(route) - 1):
+            city1 = route[i]
+            city2 = route[(i + 1) % len(route)]
+            total += self.distance(city1, city2)
+        return total
+
+    def generate_solution_neighbor(self, solution):
+        new_solution = solution.copy()
+
+        index1, index2 = random.randint(0, len(self.cities) - 1), random.randint(0, len(self.cities) - 1)
+        new_solution[index1], new_solution[index2] = new_solution[index2], new_solution[index1]
+
+        return new_solution
+
+    def path_between_cities(self, start, end):
+        queue = [(0, start, [])]
+        visited = set()
+
+        while queue:
+            cost, city, path = heapq.heappop(queue)
+
+            if city == end:
+                return path + [city]
+
+            if city in visited:
+                continue
+
+            visited.add(city)
+
+            for neighbor, edge_cost in self.map[city]:
+                if neighbor not in visited:
+                    heapq.heappush(queue, (cost + edge_cost, neighbor, path + [city]))
+
+        return None
+
+    def run(self, iterations=100_000):
+        current_solution = self.generate_initial_solution()
+        current_total_distance = self.calculate_total_distance(current_solution)
+
+        for _ in range(iterations):
+            new_solution = self.generate_solution_neighbor(current_solution)
+            new_solution_distance = self.calculate_total_distance(new_solution)
+
+            if new_solution_distance < current_total_distance:
+                current_solution = new_solution
+                current_total_distance = new_solution_distance
+
+        answer_solution = []
+        for i in range(len(current_solution) - 1):
+            path = self.path_between_cities(current_solution[i], current_solution[i + 1])
+            answer_solution.extend(path)
+            answer_solution.pop()
+
+        return answer_solution, current_total_distance
+
+
+if __name__ == "__main__":
+    graph = {
+        'Arad': [('Zerind', 75), ('Timisoara', 118), ('Sibiu', 140)],
+        'Zerind': [('Arad', 75), ('Oradea', 71)],
+        'Timisoara': [('Arad', 118), ('Lugoj', 111)],
+        'Sibiu': [('Arad', 140), ('Fagaras', 99), ('Rimnicu Vilcea', 80), ('Oradea', 151)],
+        'Oradea': [('Zerind', 71), ('Sibiu', 151)],
+        'Lugoj': [('Timisoara', 111), ('Mehadia', 70)],
+        'Fagaras': [('Sibiu', 99), ('Bucharest', 211)],
+        'Rimnicu Vilcea': [('Sibiu', 80), ('Pitesti', 97), ('Craiova', 146)],
+        'Mehadia': [('Lugoj', 70), ('Dobreta', 75)],
+        'Bucharest': [('Fagaras', 211), ('Pitesti', 101), ('Giurgiu', 90), ('Urziceni', 85)],
+        'Pitesti': [('Rimnicu Vilcea', 97), ('Bucharest', 101), ('Craiova', 138)],
+        'Craiova': [('Rimnicu Vilcea', 146), ('Pitesti', 138), ('Dobreta', 120)],
+        'Dobreta': [('Mehadia', 75), ('Craiova', 120)],
+        'Giurgiu': [('Bucharest', 90)],
+        'Urziceni': [('Bucharest', 85), ('Hirsova', 98), ('Vaslui', 142)],
+        'Hirsova': [('Urziceni', 98), ('Eforie', 86)],
+        'Vaslui': [('Urziceni', 142), ('Iasi', 92)],
+        'Iasi': [('Vaslui', 92), ('Neamt', 87)],
+        'Neamt': [('Iasi', 87)],
+        'Eforie': [('Hirsova', 86)]
+    }
+    cities = list(graph.keys())
+
+    solver = HillClimbingAlgorithm(graph, cities)
+    print(solver.run())
